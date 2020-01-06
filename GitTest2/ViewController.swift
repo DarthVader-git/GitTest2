32fe97638dfa2a08bdd5457c85705b28de94fea4
Shubham Gupta <sgshubh157@gmail.com>
2020-01-06 18:36:33 +0530

    Revert

--- GitTest2/ViewController.swift
+++ GitTest2/ViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("Welcome to Github")
-        print("Practicing Commit")
         super.viewDidLoad()
        
     }
